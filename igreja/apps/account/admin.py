b6b8b55bee99dd680695789564538e8ac1a9421b
Danilo da Conceição Nascimento <daconnas.dcn@gmail.com>
2022-03-26 09:51:40 -0300

    creating a custom user model and manager

--- igreja/apps/account/admin.py
+++ igreja/apps/account/admin.py
@@ -1,3 +1,28 @@
+from apps.account.models import Address, CustomUser, Profile
 from django.contrib import admin
+from django.http.response import JsonResponse
+from django.urls import path
 
-# Register your models here.
+
+class AddressAdmin(admin.ModelAdmin):
+    list_filter = ["id"]
+    search_fields = ["id"]
+
+    def has_view_permission(self, request, obj=None):
+        return True
+
+
+class ProfileAdminInline(admin.StackedInline):
+    model = Profile
+    autocomplete_fields = ["address"]
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["id", "__str__"]
+    list_filter = ["is_superuser", "is_staff"]
+
+    inlines = [ProfileAdminInline]
+
+
+admin.site.register(Address, AddressAdmin)
+admin.site.register(CustomUser, UserAdmin)
