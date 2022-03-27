46a7b93d4a6b4777540c1761e960e6bc4e442b34
Danilo da Conceição Nascimento <daconnas.dcn@gmail.com>
2022-03-27 16:27:39 -0300

    add blog

--- igreja/settings.py
+++ igreja/settings.py
@@ -40,10 +40,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django_registration",
     "rest_framework",
-    "apps.core",
+    "apps.blog",
     "apps.account",
+    "apps.core",
 ]
 
 MIDDLEWARE = [
